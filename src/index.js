36221d8abeb0a08257d488d033c59371b1fd8120
eve337 <vesunchik1945@gmail.com>
2021-01-26 10:36:42 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+
+  let temp = n < 0 ? n * (-1) : n;
+  temp = temp.toString().split("").reverse().join("");
   
+  if(n < 0 && Number(temp) > 0 && n > Number(temp)){
+    return Number(temp)*(-1);
+  } else if (n < 0 && Number(temp) > 0 && n < Number(temp)){
+    return Number(temp);
+  }
 }
